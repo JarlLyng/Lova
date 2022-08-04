d9ec675389f78f4ce7930fdfd4a7d717758a3d50
Jarl Lyng <38857800+JarlLyng@users.noreply.github.com>
2022-08-04 22:29:19 +0200

    added weeks

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ let Difference_In_Time = date_Todays_date.getTime() - date_Birthday.getTime();
 // To calculate the no. of days between two dates
 let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
 
+// To calculate the weeks
+let Difference_In_Weeks = Difference_In_Days / 7;
+
 // To calculate the years
 let Difference_In_Years = Math.ceil(Difference_In_Days) / 365;
 
@@ -16,6 +19,8 @@ document.getElementById("age").innerHTML =
   "is " +
   Difference_In_Years.toFixed(2) +
   " Years old" +
-  " (Thats " +
+  " (" +
   Math.ceil(Difference_In_Days) +
-  " days old)";
+  " days / " +
+  Difference_In_Weeks.toFixed(2) +
+  " Weeks)";
